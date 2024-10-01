81cad1496428308b90905168e32ee0470eefe1ab
lancelotravier <lancelotravier@gmail.com>
2024-10-01 08:26:22 +0200

    Add files via upload
    add card + images

--- carte.jl
+++ carte.jl
@@ -1,21 +1,49 @@
-@oodef mutable struct carte
-    nom::String
-    suit::String
-    valeur::Int
-    
-    function new(nom::String,suit::String, valeur::Int)
-        self = @mk
-        self.nom = nom
-        self.suit = suit
-        self.valeur = valeur
-        return self
-    end
 
-    function renvoie_valeur(self)
-        print(self.valeur)
+using Random
+using Images
+
+suits = ["clubs","spades","hearts","diamonds"]
+ranks = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"]
+
+card_value = Dict(
+    "ace" => 1,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "10" => 10,
+    "jack" => 10,
+    "dame" => 10,
+    "roi" => 10
+)
+
+function create_deck()
+    deck = []
+    for suit in suits
+        for rank in ranks
+            image_path = "images/$(rank)_of_$(suit).png"
+            push!(deck, (rank, suit, card_value[rank]))
+        end
     end
+    return deck
 end
 
-carte_ex = carte("Deux",2)
-carte.renvoie_valeur()
+function create_blackjack_deck(num_decks=6)
+    full_deck = []
+    for _ in 1:num_decks
+        append!(full_deck, create_deck())
+    end
+    return full_deck
+end
 
+blackjack_deck = create_blackjack_deck()
+
+play_deck = shuffle!(blackjack_deck)
+
+for card in play_deck[1:10]
+    println(card)
+end

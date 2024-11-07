276d3a69a97371a0513e6f441f162512ae30ff86
lancelotravier <lancelotravier@gmail.com>
2024-11-07 09:13:04 +0100

    cpp benchmark

---
+++ Benchmark/benchmark_cpp/benchmark_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  benchmark_cpp
+//
+//  Created by Lancelot on 07/11/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

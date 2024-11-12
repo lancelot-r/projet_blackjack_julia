77cd432bea3d142aa09ec001046fc19e5b544ccd
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-11-12 11:03:46 +0100

    nouveau notebook pas fini

---
+++ balckjack_notebook.jl
@@ -0,0 +1,26 @@
+### A Pluto.jl notebook ###
+# v0.20.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 608ff810-a0d9-11ef-2668-79a348c1b5b2
+begin
+	import Pkg
+	Pkg.activate("..")
+	using Vizagrams, PlutoUI, HypertextLiteral
+	using Toto_jack
+	game = initialize_game()
+	nothing
+end
+
+# ╔═╡ 37dc8fd4-ec5b-40a8-a01b-87e2cac657d7
+
+
+# ╔═╡ 412f7f86-e375-4be5-856a-05ab7b16c93e
+
+
+# ╔═╡ Cell order:
+# ╠═608ff810-a0d9-11ef-2668-79a348c1b5b2
+# ╠═37dc8fd4-ec5b-40a8-a01b-87e2cac657d7
+# ╠═412f7f86-e375-4be5-856a-05ab7b16c93e
---
+++ toto_jack.jl
@@ -0,0 +1,9 @@
+module TotoJack
+
+using Vizagrams, Pluto, PlutoUI, HypertextLiteral
+
+export Game, initialize_game, new!, turn!, end_game!, interaction
+
+include("jeu_pluto_notebook.jl")
+
+end
